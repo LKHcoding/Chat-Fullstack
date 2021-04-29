53611be6e8d8a8b0251412c1ecbf4111912f483b
Kunhee Lim <tha920@naver.com>
2021-04-29 20:56:38 +0900

    App.tsx 라우팅 처리
    -> app.tsx 라우팅 처리 완료

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import loadable from '@loadable/component';
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
-  return <div>좀만 참아주세요 ㅠㅠ</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
